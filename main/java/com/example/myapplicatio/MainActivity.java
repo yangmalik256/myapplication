62446ad350d25972da608d83cd28a0f41221876c
yangmalik256 <104385279+yangmalik256@users.noreply.github.com>
2023-02-06 05:51:07 -0800

    Add files via upload

---
+++ main/java/com/example/myapplicatio/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplicatio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World!");
+        setContentView(textView);
+    }
+}
